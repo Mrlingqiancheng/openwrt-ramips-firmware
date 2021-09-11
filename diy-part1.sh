ec843c5cb49f1c45c033d18cb47056fb8a8d7efc
Mrlingqiancheng <1977598467@qq.com>
2021-09-11 16:23:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git netkeeper https://github.com/CCnut/feed-netkeeper/tree/LUCI-LUA-UCITRACK.git' >>feeds.conf.default
+echo 'src-git https://github.com/CCnut/feed-netkeeper/tree/LUCI-LUA-UCITRACK.git' >>feeds.conf.default
